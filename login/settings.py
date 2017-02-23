630a2bdd25b7369fb1ae53224bce843ebec909b7
jquintero9 <jhjaquintero@gmail.com>
2017-02-23 00:07:56 -0500

    Update settings.py

--- login/settings.py
+++ login/settings.py
@@ -155,7 +155,5 @@ SOCIALACCOUNT_PROVIDERS = {
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 25
-EMAIL_HOST_USER = 'psychologytestutp@gmail.com'
-EMAIL_HOST_PASSWORD = 'proyecto_labsoft'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
